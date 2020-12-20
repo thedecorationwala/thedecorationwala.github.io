7fc0fa21f37a14abbbe08d055afc496cebbfdab2
unknown <thedecorationwala.contact@gmail.com>
2020-12-20 11:56:17 +0530

    Uploading Project

---
+++ js/styleSwitcher.js
@@ -0,0 +1,33 @@
+
+const links = document.querySelectorAll('.alternate-style'),
+    totalLinks = links.length;
+
+function setActiveStyle(color)
+{
+    for (let i = 0; i < totalLinks; i++) {
+        if (color === links[i].getAttribute('title')) {
+            links[i].removeAttribute('disabled');
+        } else {
+            links[i].setAttribute('disabled', true);
+        }
+    }
+}
+
+// body skin
+
+const bodySkin = document.querySelectorAll('.body-skin'),
+    totalBodySkin = bodySkin.length;
+
+for (let i = 0; i < totalBodySkin; i++) {
+    bodySkin[i].addEventListener('change', function(){
+        if(this.value === 'light'){
+            document.body.className = '';
+        } else {
+            document.body.className = 'dark';
+        }
+    });
+}
+
+document.querySelector('.toggle-style-switcher').addEventListener('click', () => {
+    document.querySelector('.style-switcher').classList.toggle('open');
+});
